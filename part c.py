6e4a11984a59d91099deaff6ac2f542972915c67
myraejie <b00128714@mytudublin.ie>
2025-02-10 18:24:56 +0000

    Completed Part c

---
+++ part c.py
@@ -0,0 +1,17 @@
+#Myra Ejie
+#10/02/2025
+#A Program to Print Steps 1 to 9 to the Screen
+
+def printSteps(a,b):
+    print("step 4")
+    print("step 5")
+    print("step 6")
+
+
+print("step 1")
+print("step 2")
+print("step 3")
+printSteps(4,6)
+print("step 7")
+print("step 8")
+print("step 9")
